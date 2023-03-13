27ad1407c3a4f44bc2cc965872e0dbec9585f945
Jonas Hahn <herzock@gmail.com>
2023-03-13 21:19:18 +0100

    Add selected asset to transfer popup

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -235,6 +235,7 @@ export default function Home() {
           <TransferAdressInput
             onCancelCallback={onTransferCancel}
             onTransferCallback={onTransferSubmit}
+            assetId={selectedAsset}
           />
         </div>
       )}
--- next/src/components/TransferAdressInput.tsx
+++ next/src/components/TransferAdressInput.tsx
@@ -4,6 +4,7 @@ type Props = {
   children?: React.ReactNode;
   onTransferCallback: (target: string) => void;
   onCancelCallback: () => void;
+  assetId: string;
 };
 
 export default function TransferAdressInput(props: Props) {
@@ -15,7 +16,10 @@ export default function TransferAdressInput(props: Props) {
 
   return (
     <div className="w-full h-full flex justify-center items-center backdrop-brightness-50">
-      <div>
+      <div className="bg-red-700 rounded-lg border-4 px-4 py-4">
+      <h1 className="text-sky-400 text-3xl font-bold">Transfer NFT:</h1>
+      <h6 className="text-s">{props.assetId}</h6>
+      <h1 className="text-sky-400 text-3xl font-bold">to:</h1>
       <input type="text" id="target_address" value={value} onChange={handleChange}  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Address" required>
       </input>
       <div>
