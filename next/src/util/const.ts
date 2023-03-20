ca4cd379140fb46d939c5826666cc2f0f6367caf
Jonas Hahn <herzock@gmail.com>
2023-03-20 23:51:28 +0100

    Change collection to mainnet

--- next/src/util/const.ts
+++ next/src/util/const.ts
@@ -11,16 +11,23 @@ export const CONNECTION = new WrappedConnection(process.env.NEXT_PUBLIC_RPC ? pr
     commitment: 'confirmed' 
   });
 
-  export const CollectionMint = new PublicKey("7gZEKGK9V7iFRF79F3otjBp7jMuejMLirvovcgNofAhX");
+  export const CollectionMint = new PublicKey("G8sPwd2jj4UXXyvTHKd6awFhi4aUQUZ7cbB3KRsPuXJU");
   export const TreeAccount = new PublicKey("tr4YwToCp79vnapocnujb6KbZpcbcRKdntBqgFW459P");
 
-  export const collectionMetadataAccount = new PublicKey("HqAYQfkyMKDG5h4kUTaWCh45AYRHF95KHstYhgcf4Dy7");
-  export const collectionMasterEditionAccount = new PublicKey("2jF5C8yErRtp52CgYY9YpuaYLQkS3gQ5UHWATUZzQxj3");
+  export const collectionMetadataAccount = new PublicKey("7ns3EDsE1qaysniaR4guyBWihzmcCPisGRdpL262pmgU");
+  export const collectionMasterEditionAccount = new PublicKey("n1kf6hEFtBd2ncY2MrMFffMN69LzjSVjScTTWrRGVZ7");
 
   /*
     ===Collection Details===
     Mint account: 7gZEKGK9V7iFRF79F3otjBp7jMuejMLirvovcgNofAhX
     Metadata account: HqAYQfkyMKDG5h4kUTaWCh45AYRHF95KHstYhgcf4Dy7
     Master edition account: 2jF5C8yErRtp52CgYY9YpuaYLQkS3gQ5UHWATUZzQxj3
+
+    Main net
+
+===Collection Details===
+Mint account: G8sPwd2jj4UXXyvTHKd6awFhi4aUQUZ7cbB3KRsPuXJU
+Metadata account: 7ns3EDsE1qaysniaR4guyBWihzmcCPisGRdpL262pmgU
+Master edition account: n1kf6hEFtBd2ncY2MrMFffMN69LzjSVjScTTWrRGVZ7
   */
 
